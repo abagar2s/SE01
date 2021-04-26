9bacb2e1ba262ecba56f318470792552654bd61b
aymane <46760057+abagar2s@users.noreply.github.com>
2021-04-26 23:13:41 +0200

    gerade geändert
    testklassee

--- junit-5-basics/src/test/java/TabelleImplTest.java
+++ junit-5-basics/src/test/java/TabelleImplTest.java
@@ -2,6 +2,12 @@ import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.fail;
 
+<<<<<<< Updated upstream
+=======
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
+>>>>>>> Stashed changes
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
@@ -23,9 +29,31 @@ class TabelleImplTest {
 	@Test
 	void test4() {
 		
+<<<<<<< Updated upstream
 	    char[] output = test4.isWellStored(c4);
 		char[] a = {'F','B','A','C','D'} ;
 		assertArrayEquals(a,output);
+=======
+		sequence = new TabelleImpl();
+		//String[] output = sequence.Sorted(ad);
+		String a = "FBACD" ;
+		String b = "FABCD" ;
+		String c = "ABFCD" ;
+		String d = "AFBCD" ;
+		String e = "BAFCD" ;
+		String f = "BFACD" ;
+		String g = "BFADC" ;
+
+		//char[][] output = {{'F','B','A','C','D'}, {'F','A','B','C','D'},{'A','B','F','C','D'},{'A','F','B','C','D'},{'B','A','F','C','D'}, {'B','F','A','C','D'}};
+		String[] output1 = {a,b,c,d,e,f};
+		for (String hs : output1) {
+			
+		//boolean contains = Arrays.contains(output1, a);
+		
+		assertTrue(Arrays.asList(output1).contains(hs));
+		}
+		
+>>>>>>> Stashed changes
 	}
 	@Test
 	void test1() {
